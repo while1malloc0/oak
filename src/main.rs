9a76ebd4aa590c8bbc05552e5a594c5d92dbdee7
John Turner <johnturner@me.com>
2022-03-26 21:46:24 +0000

    wip: add failing e2e test for noargs,noflags

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    println!("not yet implemented")
+}
+
+mod tests {
+    use super::*;
+    use assert_cmd::Command;
+
+    #[test]
+    fn test_noargs_noflags() {
+        let mut subject = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+        let assert = subject
+            .current_dir(env::current_dir().unwrap().join("src/testdata"))
+            .write_stdin("noargs")
+            .assert();
+        let want = include_str!("testdata/noargs/want.txt");
+        assert.success().stdout(want);
+    }
 }
